9a42000f99f53d8ee4f0fb503110eef3d7fa4058
Nzingani <isabelnzingani@outlook.com>
2018-07-25 21:37:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>exo4</title>
+  </head>
+  <body>
+  <p>
+    <?php
+    $string = 'Bonjour';
+    echo $string;
+    ?>
+  </p>
+  <p>
+    <?php
+    $init = 3;
+    echo $init;
+    ?>
+  </p>
+  <p>
+    <?php
+    $float = 12.58;
+    echo $float;
+    ?>
+  </p>
+  <p>
+    <?php
+    $bool = true;
+    echo $bool;
+    ?>
+  </p>
+  </body>
+</html>
